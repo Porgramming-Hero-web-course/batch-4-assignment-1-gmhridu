9ec8754ed9c7272345e3e45415532ed668ca4326
gmhridu <hasanhridoymahabub9@gmail.com>
2024-11-08 11:05:19 +0600

    problem-6 solved

---
+++ problem-6.ts
@@ -0,0 +1,26 @@
+{
+    //
+    interface Profile
+    {
+        name: string
+        age: number
+        email: string
+    }
+
+    function updateProfile ( profile: Profile, updates: Partial<Profile> ): Profile
+    {
+        return {...profile, ...updates}
+    }
+
+    const myProfile = {
+        name: "Alice",
+        age: 25,
+        email: "alice@example.com"
+    }
+
+    const profileUpdate = updateProfile( myProfile, { age: 26 } )
+    console.log(profileUpdate)
+
+
+    //
+}
